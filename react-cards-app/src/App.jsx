9a114090f294886f6a6fd931b21f82b97fbacc06
Brittani Lovelace <blovelace84@gmail.com>
2025-02-21 18:53:41 -0500

    deleted index.html and added title to app component

--- react-cards-app/src/App.jsx
+++ react-cards-app/src/App.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React,{ useEffect } from "react";
 import PlayingCardList from "./PlayingCardList";
 import PokeDex from "./PokeDex";
 import "./App.css"; // Import the CSS file
 
 function App() {
+  useEffect(() => {
+    document.title = "React Cards";
+  }, []);
   return (
     <div className="app-container">
       <h1 className="app-title">React Cards</h1>
