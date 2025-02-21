ebfbd0c35f335c2cd57fee96bc662f37e99687c3
Brittani Lovelace <blovelace84@gmail.com>
2025-02-21 18:36:14 -0500

    Update PokeDex with styles

--- react-cards-app/src/PokeDex.jsx
+++ react-cards-app/src/PokeDex.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import useAxios from "./useAxios";
 import PokemonCard from "./PokemonCard";
+import "./PokeDex.css"; // Import the CSS file
 
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 function PokeDex() {
-  const [pokemon, addPokemon] = useAxios(BASE_URL);
+  const [pokemon, addPokemon, clearPokemon] = useAxios(BASE_URL);
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleAddPokemon = () => {
@@ -16,22 +17,30 @@ function PokeDex() {
   };
 
   return (
-    <div>
+    <div className="pokedex-container">
+      <h1>Gotta Catch Em All!! Pokemon PokeDex</h1>
       <input
         type="text"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="Enter Pokémon name"
+        className="pokedex-input"
       />
-      <button onClick={handleAddPokemon}>Fetch Pokémon</button>
+      <button onClick={handleAddPokemon} className="pokedex-button">
+        Fetch Pokémon
+      </button>
+      <button onClick={clearPokemon} className="pokedex-button">
+        Release Pokémon
+      </button>
 
-      <div>
+      <div className="pokedex-pokemon-list">
         {pokemon.length > 0 ? (
           pokemon.map((p, idx) => (
             <PokemonCard
               key={idx}
               name={p.name}
               image={p.sprites.front_default}
+              className="pokedex-pokemon-card"
             />
           ))
         ) : (
