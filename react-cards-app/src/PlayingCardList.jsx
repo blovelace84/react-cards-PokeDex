5c505837ece20a4e31049c5418dd09c1007eb766
Brittani Lovelace <blovelace84@gmail.com>
2025-02-21 12:46:45 -0500

    Styles for Playing card list component

--- react-cards-app/src/PlayingCardList.jsx
+++ react-cards-app/src/PlayingCardList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import useAxios from "./useAxios";
 import PlayingCard from "./PlayingCard";
+import "./PlayingCardList.css";
 
 const BASE_URL = "https://deckofcardsapi.com/api/deck/new/draw/";
 
@@ -8,13 +9,13 @@ const PlayingCardList = () => {
   const [cards, addCard, clearCards] = useAxios(BASE_URL);
 
   return(
-    <div>
-      <button onClick={() => addCard()}>Draw a Card</button>
-      <button onClick={clearCards}>Reset</button>
-      <div>
+    <div className="container">
+      <button className="button" onClick={() => addCard()}>Draw a Card</button>
+      <button className="button" onClick={clearCards}>Reset</button>
+      <div className="cards">
         {cards.length > 0 ? (
           cards.map((card, idx) => (
-            <PlayingCard key={idx} image={card.cards[0].image} />
+            <PlayingCard key={idx} image={card.cards[0].image} className="card" />
           ))
         ) : (
           <p>No cards drawn yet.</p>
